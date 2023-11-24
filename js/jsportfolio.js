e7f86425f69febc3e2ad6af9978283d4db4f70e3
Tinkjimmy <tinkjimmy938@gmail.com>
2023-11-25 00:42:24 +0100

    fixing issues

--- js/jsportfolio.js
+++ js/jsportfolio.js
@@ -1,8 +1,22 @@
 
 
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarCollapse = document.querySelector('.navbar-collapse');
+// const navbarToggler = document.querySelector('.navbar-toggler');
+// const navbarCollapse = document.querySelector('.navbar-collapse');
 
-navbarToggler.addEventListener('click', () => {
-  navbarCollapse.classList.toggle('show');
+// navbarToggler.addEventListener('click', () => {
+//   navbarCollapse.classList.toggle('show');
+// });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+  const navbarIcon = document.querySelector('.navbar-toggler-icon');
+  const navbar = document.querySelector('.navbar');
+
+  navbarToggler.addEventListener('click', () => {
+    navbarCollapse.classList.toggle('show');
+    navbarIcon.classList.toggle('rotated');
+    navbar.classList.toggle('show');
+  });
 });
